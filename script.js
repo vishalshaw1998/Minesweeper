e6fca055ac7b7e5e28e1cbc569422bc26b3e0a6c
Vishal <vishalshaw1998@gmail.com>
2020-06-25 14:05:35 +0530

    Landing page

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ var container = document.createElement("div");
 container.setAttribute("class", "container");
 
 divForText = document.createElement("div");
-divForText.setAttribute("class", "text-center");
+divForText.setAttribute("class", "text-center padding-top font-krona");
 divForText.innerHTML = "<span>Lets Play Minesweeper</span>";
 
 var divForButton = document.createElement("div");
-divForButton.setAttribute("class", "text-center");
-var btn = document.createElement("button");
-btn.innerHTML = "<a href = '#'>Play</a>";
-divForButton.appendChild(btn);
+divForButton.setAttribute("class", "text-center margin-top");
+var a = document.createElement("a");
+a.setAttribute("href", "#");
+a.innerHTML = "Click to Play";
+divForButton.appendChild(a);
 
 container.appendChild(divForText);
 container.appendChild(divForButton);
